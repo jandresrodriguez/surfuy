c26d85a76e89df4b80b50891dd24ed2bc6117bd1
Juan Andres Rodriguez <jandres.rodriguezg@gmail.com>
2015-08-17 22:36:30 -0300

    Authentication requirements added

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  devise_scope :user do
-    root to: "devise/sessions#new"
-  end
+  root to: "home#index"
   
 
   # Example of regular route:
